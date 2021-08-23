56ffd2977a0e40ba496c00a21daa13c3bd1ad913
weepingdogel <weepingdogel@gmail.com>
2021-08-23 22:40:49 +0800

    Redo

--- BackEnd/server.py
+++ BackEnd/server.py
@@ -1,17 +1,11 @@
-from flask import Flask
-import os
+'''
+Libredanmaku|Server
+
+By WeepingDogel
+
+Selecting the Web frameworks now...
+'''
+
 import toml
 
-class Appearance:
-    def ShowLogo(self,config):
-        print(config['Appearance']['logo'])
-class Initialization:
-    def LoadConfig(self,filename):
-        print("Loading Config")
-        out = toml.load(filename)
-        return out
-def main():
-    A = Appearance()
-    Init =Initialization()
-    config = Init.LoadConfig('config.toml')
-    A.ShowLogo(config)
+
